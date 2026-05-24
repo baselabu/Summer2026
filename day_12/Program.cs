8f35742c7ec032f9b1c813873bf01e794478138f
baselabu <baseltv11@gmail.com>
2026-05-24 17:32:09 +0200

    Learned LINQ and applied it in a list to learn how it works

---
+++ day_12/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Immutable;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
+List<int> numbers = new List<int>{1, 34, 3, 4, 5};
+var EvenNumbers = numbers.Where(x => x % 2 == 0);
+var MaxNumber = numbers.Max();
+var sumNumbers = numbers.Sum();
+System.Console.WriteLine(MaxNumber);
+System.Console.WriteLine(sumNumbers);
+foreach(int number in EvenNumbers)
+{
+    System.Console.WriteLine(number);
+}
