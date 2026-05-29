b1834a5a479e016e6284989a813177fd987f3c21
baselabu <baseltv11@gmail.com>
2026-05-29 21:10:01 +0200

    frequincy of each letter in a word

---
+++ PHASE 2 — Data Structures & Problem Solving (Days 15–28)/Week_3/day_17/Program.cs
@@ -0,0 +1,19 @@
+﻿string word = "Hello, World!";
+
+Dictionary<string, int> letterCounts = new Dictionary<string, int>();
+foreach (char letter in word)
+{
+    string letterStr = letter.ToString();
+    if (letterCounts.ContainsKey(letterStr))
+    {
+        letterCounts[letterStr]++;
+    }
+    else
+    {
+        letterCounts[letterStr] = 1;
+    }
+}
+Console.WriteLine("Letter counts:");
+foreach (var kvp in letterCounts)
+{    Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+}
