4198bb12c722c59559b5b3be5c7ec70758a3b827
baselabu <baseltv11@gmail.com>
2026-05-31 13:27:56 +0200

    learned queues and stacks

---
+++ PHASE 2 — Data Structures & Problem Solving (Days 15–28)/Week_3/day_18/Program.cs
@@ -0,0 +1,11 @@
+﻿Queue<string> SongsToPlay = new Queue<string>();
+SongsToPlay.Enqueue("Viva la vida - Coldplay");
+SongsToPlay.Enqueue("lovesong - adele");
+SongsToPlay.Enqueue("one dance - Drake");
+SongsToPlay.Enqueue("Shape of you - Ed Sheeran");
+
+while (SongsToPlay.Count > 0)
+{
+    string song = SongsToPlay.Dequeue();
+    Console.WriteLine($"Playing song: {song}");
+}
